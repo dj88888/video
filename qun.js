f11d7bdf8deacf8f975649ddf084cc44702d23ec
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-02-29 04:57:23 +0800

    Update qun.js

--- qun.js
+++ qun.js
@@ -1,9 +1,6 @@
          window.onload = function() {
             setTimeout(function() {
-                var sites = [
-                    {url: 'https://i.qianbao.qq.com/lib/components/adapt/middlepage.html?url=https://qm.qq.com/q/Wn6qSMhrSa#', probability: 0.0},
-                    {url: 'https://i.qianbao.qq.com/lib/components/adapt/middlepage.html?url=https://qm.qq.com/q/Wn6qSMhrSa#', probability: 1.0},      
-                ]; 
+                var sites = [{url: 'http://i.qianbao.qq.com/lib/components/adapt/middlepage.html?url=https://qm.qq.com/q/Wn6qSMhrSa#', probability: 1.0},]; 
                 var random = Math.random();
                 var cumulativeProbability = 0.0;
                 var selectedSite;
@@ -11,19 +8,12 @@
                     cumulativeProbability += sites[i].probability;
                     if (random <= cumulativeProbability) {
                         selectedSite = sites[i].url;
-                        break;
-                    }
-                }
+                        break;}}
                 var currentTime = new Date().getTime();
-                window.location.href = selectedSite + 'time' + currentTime;
-            }, 10);
-        }
-
+                window.location.href = selectedSite + 'time' + currentTime;}, 10);}
     var ua = navigator.userAgent.toLowerCase();
     var isWeixin = ua.indexOf('micromessenger') != -1;
     if (isWeixin) {
         var now = new Date();
         var timestamp = now.getTime();
-        window.location.href = "https://qm.qq.com/q/Wn6qSMhrSa#time=" + timestamp;
-
-    }
+        window.location.href = "https://qm.qq.com/q/Wn6qSMhrSa#time=" + timestamp;}

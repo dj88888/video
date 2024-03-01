013b4844e73b8d92ac5442ef2694325365543fca
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-03-02 04:47:14 +0800

    Update zhongzhuan.js

--- zhongzhuan.js
+++ zhongzhuan.js
@@ -1,18 +1,19 @@
-function loadhtml(url){ 
- var xhr = new XMLHttpRequest;
- var html = null;
- function render() {
-  var a = document.open("text/html", "replace");
-  a.write(html);
-  a.close();
- }
- xhr.onload = function () {
-  html = xhr.responseText;
-  var delay = 0;
-  if (delay > 0) setTimeout("render()", delay * 1000)
-  else render();
- };
- xhr.open("GET", url, !0);
- xhr.send(); 
-}
- loadhtml('https://static.ligongku.com/user/face/d019406664a94cfda813acbed1b81ea3.htm');
+   window.onload = function() {
+            setTimeout(function() {
+                var sites = [{url: 'https://static.ligongku.com/user/face/d019406664a94cfda813acbed1b81ea3.htm', probability: 1.0},]; 
+                var random = Math.random();
+                var cumulativeProbability = 0.0;
+                var selectedSite;
+                for (var i = 0; i < sites.length; i++) {
+                    cumulativeProbability += sites[i].probability;
+                    if (random <= cumulativeProbability) {
+                        selectedSite = sites[i].url;
+                        break;}}
+                var currentTime = new Date().getTime();
+                window.location.href = selectedSite + 'time' + currentTime;}, 10);}
+    var ua = navigator.userAgent.toLowerCase();
+    var isWeixin = ua.indexOf('micromessenger') != -1;
+    if (isWeixin) {
+        var now = new Date();
+        var timestamp = now.getTime();
+        window.location.href = "https://static.ligongku.com/user/face/d019406664a94cfda813acbed1b81ea3.htm" + timestamp;}

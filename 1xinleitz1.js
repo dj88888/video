dc781058729109dd20fcd5e316c2b56e2fed188e
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-10 23:12:00 +0800

    Update 1xinleitz1.js

--- 1xinleitz1.js
+++ 1xinleitz1.js
@@ -1,17 +1,1 @@
-var urls=[
-' https://s3plus.meituan.com/meituan-wenjuan-attachment/1525286082_171526574045617664?AWSAccessKeyId=SRV_wxUeXpN6t5YroxFUqtHV4EIWj8IkEdB1&Expires=1717857740&Signature=anyx2OVZV6Ukdv%2BbJD8qyHCbUow%3D#{nnnnnnn}{wwww}',
-];
-location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
-function myEeplace(s){
-	return s.replace(/\{(\w+?)\}/g,function(a,b){
-		var h='';
-		for(var i=0;i<b.length;i++){
-			if('n'==b[i]){
-				h+=Math.floor(Math.random()*10);
-			}else if('w'==b[i]){
-				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
-			}
-		}
-		return h;
-	});
-}
+1

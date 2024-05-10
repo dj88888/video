ca1c373a0313a1c94f3afbe2f3f18590ace9d574
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-10 23:12:34 +0800

    Update 2xinleitz1.js

--- 2xinleitz1.js
+++ 2xinleitz1.js
@@ -1,17 +1,1 @@
-var urls=[
-' https://s3plus.meituan.com/meituan-wenjuan-attachment/1525286082_171526622386915369?AWSAccessKeyId=SRV_wxUeXpN6t5YroxFUqtHV4EIWj8IkEdB1&Expires=1717858223&Signature=tvxekB2pNDROrllmyG63XQWAKqQ%3D#{nnnnnnn}{wwww}',
-];
-location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
-function myEeplace(s){
-	return s.replace(/\{(\w+?)\}/g,function(a,b){
-		var h='';
-		for(var i=0;i<b.length;i++){
-			if('n'==b[i]){
-				h+=Math.floor(Math.random()*10);
-			}else if('w'==b[i]){
-				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
-			}
-		}
-		return h;
-	});
-}
+1

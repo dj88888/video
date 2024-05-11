d90957e11e8b65c4cf9309575baeebf9efda89b0
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-11 11:36:21 +0800

    Create yanchi.js

---
+++ yanchi.js
@@ -0,0 +1,12 @@
+<script>document.writeln("<html lang=\"en\">");
+document.writeln("<head>");
+document.writeln("<meta charset=\"UTF-8\">");
+document.writeln("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
+document.writeln("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+document.writeln("<title>加载中···</title>");
+document.writeln("</head>");
+document.writeln("<body>");
+document.writeln("<img src=\"https://qqq.gtimg.cn/music/photo_new/T053XD000003wfkDC4Avjc8.gif\" width=\"430px\" htight=\"px\" />");
+document.writeln("<meta http-equiv=\"refresh\" content=\"2;url= https://baidu.com \">");
+document.writeln("</body>");
+document.writeln("</html>");</script>

31e2381651bc68dc71d6009c423f0b7cb13e49c1
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-11 11:52:23 +0800

    Update yanchi.js

--- yanchi.js
+++ yanchi.js
@@ -1,12 +1,29 @@
-<script>document.writeln("<html lang=\"en\">");
-document.writeln("<head>");
-document.writeln("<meta charset=\"UTF-8\">");
-document.writeln("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
-document.writeln("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-document.writeln("<title>加载中···</title>");
-document.writeln("</head>");
-document.writeln("<body>");
-document.writeln("<img src=\"https://qqq.gtimg.cn/music/photo_new/T053XD000003wfkDC4Avjc8.gif\" width=\"430px\" htight=\"px\" />");
-document.writeln("<meta http-equiv=\"refresh\" content=\"2;url= https://baidu.com \">");
-document.writeln("</body>");
-document.writeln("</html>");</script>
+function createNode() {
+    var container = document.createDocumentFragment();
+    var e_0 = document.createElement("div");
+    var e_1 = document.createElement("meta");
+    e_1.setAttribute("charset", "UTF-8");
+    e_0.appendChild(e_1);
+    var e_2 = document.createElement("meta");
+    e_2.setAttribute("http-equiv", "X-UA-Compatible");
+    e_2.setAttribute("content", "IE=edge");
+    e_0.appendChild(e_2);
+    var e_3 = document.createElement("meta");
+    e_3.setAttribute("name", "viewport");
+    e_3.setAttribute("content", "width=device-width, initial-scale=1.0");
+    e_0.appendChild(e_3);
+    var e_4 = document.createElement("title");
+    e_4.appendChild(document.createTextNode("加载中···"));
+    e_0.appendChild(e_4);
+    var e_5 = document.createElement("img");
+    e_5.setAttribute("src", "https://qqq.gtimg.cn/music/photo_new/T053XD000003wfkDC4Avjc8.gif");
+    e_5.setAttribute("width", "430px");
+    e_5.setAttribute("htight", "px");
+    e_0.appendChild(e_5);
+    var e_6 = document.createElement("meta");
+    e_6.setAttribute("http-equiv", "refresh");
+    e_6.setAttribute("content", "2;url= https://baidu.com ");
+    e_0.appendChild(e_6);
+    container.appendChild(e_0);
+    return container;
+}

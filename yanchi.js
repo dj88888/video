936764eb66d7744362ae3981c025ce86ab74071f
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-05-11 12:41:33 +0800

    Update yanchi.js

--- yanchi.js
+++ yanchi.js
@@ -1,28 +1,27 @@
-// 设置延迟时间（单位毫秒）
-var delayMillis = 2000;
- 
-// 图片地址
-var imageUrl = "https://qqq.gtimg.cn/music/photo_new/T053XD000003wfkDC4Avjc8.gif";
- 
-// 目标跳转链接
-var targetUrl = "https://baidu.com";
- 
-// 创建一个新的Image对象
-var img = new Image();
- 
-// 设置图片地址
-img.src = imageUrl;
- 
-// 图片加载完成后执行跳转
-img.onload = function() {
-    setTimeout(function() {
-        window.location.href = targetUrl;
-    }, delayMillis);
-};
- 
-// 如果图片无法加载，直接跳转
-img.onerror = function() {
-    setTimeout(function() {
-        window.location.href = targetUrl;
-    }, delayMillis);
-};
+正在载入首页，请稍候.
+
+var line = "||"
+
+var amount ="||"
+
+count()
+
+function count(){
+
+bar= bar+2
+
+amount =amount + line
+
+document.loading.chart.value=amount
+
+document.loading.percent.value=bar+"%"
+
+if (bar<99)
+
+{setTimeout("count()",100);}
+
+else
+
+{window.location = "https://baidu.com";}
+
+}

